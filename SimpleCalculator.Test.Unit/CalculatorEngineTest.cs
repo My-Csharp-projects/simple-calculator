851d84b6ef2ebbc27f51f6a2f10c2a99c0161752
Jasterix <ljcalixte@gmail.com>
2021-01-25 16:30:21 -0500

    Create basic unit tests

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -1,14 +1,50 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using simple_calculator;
 
 namespace SimpleCalculator.Test.Unit
 {
     [TestClass]
     public class CalculatorEngineTest
     {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
         [TestMethod]
-        public void TestMethod1()
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
         {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("add", num1, num2);
+
+            Assert.AreEqual(3, result);
         }
+
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("+", num1, num2);
+
+            Assert.AreEqual(3, result);
+        }
+
+        public void MultipliesTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("multiply", num1, num2);
+
+            Assert.AreEqual(2, result);
+        }
+
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("*", num1, num2);
+
+            Assert.AreEqual(2, result);
+        }
+
+
     }
 }
--- simple calculator/InputConverter.cs
+++ simple calculator/InputConverter.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace simple_calculator
 {
-    class InputConverter
+    public class InputConverter
     {
         public double ConvertInputToNumeric(string argTextInput)
         {
